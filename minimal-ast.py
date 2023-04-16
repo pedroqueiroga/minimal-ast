99cf4b42d6bb36fd5f8031ec480fa8963c2e9e11
Pedro de Souza Queiroga <pedrosqueiroga@gmail.com>
2023-04-16 00:41:57 -0300

    recurses until exhausts definitions

--- minimal-ast.py
+++ minimal-ast.py
@@ -1,6 +1,6 @@
 import ast
 
-with open('./example01.py') as f:
+with open('./example03.py') as f:
     source = ''.join(f.readlines())
 
 
@@ -17,14 +17,23 @@ class MinimalExtractor(ast.NodeVisitor):
 
     def extract(self, isname=False):
         visited_nodes = self.generic_visit(self.source_node, init=True)
+        for node in filter(
+            lambda n: isinstance(n, ast.FunctionDef), visited_nodes
+        ):
+            self.built_context.add(node.name)
+
         extracted_definitions = []
         for required in self.required_context:
-            extracted = NameExtractor(self.source_node, required).extract()
+            extracted = NameExtractor(
+                self.source_node, required, self.built_context
+            ).extract()
             definitions = list(
                 filter(lambda d: isinstance(d, ast.AST), extracted)
             )
             if len(definitions) > 0:
-                extracted_definitions.append(*definitions)
+                extracted_definitions.extend(definitions)
+            for definition in definitions:
+                self.built_context.add(definition.name)
 
         return ast.Module(
             self.move_imports_up(extracted_definitions + visited_nodes),
@@ -84,9 +93,10 @@ class MinimalExtractor(ast.NodeVisitor):
 
 
 class NameExtractor(ast.NodeVisitor):
-    def __init__(self, node, name):
+    def __init__(self, node, name, global_context):
         self.source_node = node
         self.name = name
+        self.global_context = global_context
 
     def extract(self):
         return self.generic_visit(self.source_node)
@@ -112,19 +122,37 @@ class NameExtractor(ast.NodeVisitor):
         return None
 
     def visit_FunctionDef(self, node):
-        if node.name == self.name:
-            return node
-        visited_nodes = self.generic_visit(node)
-        if (
-            len(list(filter(lambda x: isinstance(x, ast.AST), visited_nodes)))
-            > 0
+        if node.name != self.name:
+            return None
+
+        local_context = set()
+
+        for arg in (
+            (node.args.posonlyargs or [])
+            + (node.args.args or [])
+            + (node.args.vararg or [])
+            + (node.args.kwonlyargs or [])
+            + (node.args.kwarg or [])
         ):
-            return node
-        return None
+            local_context.add(arg.arg)
+
+        required_nodes = self.generic_visit(node)
+
+        self.global_context.add(node.name)
+
+        return node, list(
+            filter(
+                lambda n: n not in local_context
+                and n not in self.global_context,
+                required_nodes,
+            )
+        )
 
     def visit_Name(self, node):
-        if node.id == self.name and node.ctx == ast.Store:
+        if node.id == self.name and isinstance(node.ctx, ast.Store):
             return node
+        if isinstance(node.ctx, ast.Load):
+            return node.id
         return None
 
     def generic_visit(self, node):
@@ -133,8 +161,30 @@ class NameExtractor(ast.NodeVisitor):
 
         for child_node in child_nodes:
             visited = self.visit(child_node)
-            if isinstance(visited, ast.AST):
+            if isinstance(visited, ast.AST) or isinstance(visited, str):
                 visited_nodes.append(visited)
+            elif (
+                isinstance(visited, list)
+                and len(visited) > 0
+                and isinstance(visited[0], str)
+            ):
+                visited_nodes.extend(visited)
+            elif (
+                isinstance(visited, tuple)
+                and len(visited) == 2
+                and isinstance(visited[0], ast.AST)
+                and isinstance(visited[1], list)
+            ):
+                visited_nodes.append(visited[0])
+                for required in visited[1]:
+                    extracted = NameExtractor(
+                        self.source_node, required, self.global_context
+                    ).extract()
+                    definitions = list(
+                        filter(lambda d: isinstance(d, ast.AST), extracted)
+                    )
+                    if len(definitions) > 0:
+                        visited_nodes.extend(definitions)
 
         return visited_nodes
 
